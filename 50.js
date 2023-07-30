c8a028f3ef313c243f73e264b81a68b222258386
Jacob Alvarado <jacobalvarado715@gmail.com>
2023-07-30 18:07:39 -0500

    Update 50.js

--- 50.js
+++ 50.js
@@ -25,18 +25,3 @@ bttn.addEventListener('click', e =>{
         counter = 0
     }
 })
-
-const testArr = [144, 9, 4, 16, 25, 49, 81]
-// //map method will run a line of code for every element in the targeted array, then assign each updated element into a new array
-const square = (num) =>{
- console.log(Math.sqrt(num))
-}
-const squareRoot = testArr.map(square)
-// //using callbacks as the agrument of the map method, i passed in my sqrt function, which sqrts every number in my array
-
-const testArr2 = [20, 43, 15, 17, 25, 9, 10]
-const newArr = testArr2.filter(sortTest)
-function sortTest (num){
-    return num > 18
-}
-console.log(newArr)
